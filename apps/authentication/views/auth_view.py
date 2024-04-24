cd4aa1c0d121b85e85e50ca75bfa18959e71e6ff
Magno Martinez <magno.martinez@cognitionhq.com>
2024-04-23 23:19:01 -0500

    feature(users-auth): implement application structure

---
+++ apps/authentication/views/auth_view.py
@@ -0,0 +1,115 @@
+from django.contrib.auth import logout
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+
+
+from apps.users.models.user import User
+from apps.authentication.serializers.authentication_serializer import (
+    AuthenticationSerializer,
+    LogoutSerializer,
+)
+
+
+class AuthenticationViewSet(GenericViewSet):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [AllowAny]
+
+    def get_serializer_class(self):
+        if self.action == 'login':
+            return AuthenticationSerializer
+        else:
+            return LogoutSerializer
+
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        """
+        Logs in an existing user using their email and password.
+
+        Takes email and password as input.
+
+        If the provided email and password are correct,
+        returns a new refresh and access token for the user.
+        The refresh token can be used to generate new access tokens.
+        ---
+        Body:
+            {
+                "email": "str",
+                "password": "str"
+            }
+
+        responses:
+            {
+                "refresh": "refresh_token",
+                "access": "access_token",
+                "user": {
+                    "id": "user_id",
+                    "email": "email",
+                    "role": "role"
+                }
+            }
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user_data = serializer.validated_data
+
+        try:
+            user = User.objects.get(email=user_data['email'])
+        except User.DoesNotExist:
+            raise AuthenticationFailed(
+                "User with the provided email does not exist")
+
+        if not user.check_password(user_data['password']):
+            raise AuthenticationFailed("Incorrect password")
+
+        refresh = RefreshToken.for_user(user)
+
+        data = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+            "user": {
+                "id": user.id,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "email": user.email,
+                "role": user.role.name,
+            }
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['post'])
+    def logout(self, request):
+        """
+        Logs out the current user.
+
+        If the user is logged in,
+        deletes their refresh token.
+        This prevents them from generating new access tokens.
+
+        responses:
+            {
+                "detail": "Successfully logged out"
+            }
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        refresh_token = request.data.get('refresh')
+
+        if refresh_token:
+            try:
+                RefreshToken(refresh_token).blacklist()
+            except RefreshToken.InvalidToken:
+                logout(request)
+                return Response({"detail": "Refresh token is invalid"},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({"detail": "Successfully logged out"},
+                        status=status.HTTP_200_OK)
